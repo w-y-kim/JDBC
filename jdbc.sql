f4d693013e3c8d4a35c3b4cf00e5c369420e6229
김완길 <watakumo@gmail.com>
2016-05-18 13:30:23 +0900

    8.완료

--- jdbc.sql
+++ jdbc.sql
@@ -1,5 +1,5 @@
 CREATE table member(
-id varchar2(20) primary key,
+id varchar(20) primary key,
 password varchar(20) not null,
 name varchar(20) not null, 
 age number(3) 
--- jdbcTest.java
+++ jdbcTest.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class jdbcTest {
 
@@ -40,7 +41,7 @@ public class jdbcTest {
 	 * 조회하고 조회된 결과를 반환하는 메소드
 	 * 
 	 * @param id
-	 *            테이블이 만들어져야하고 테이블과 1:1 관계에 있는 VO를 만들어야 한다.
+	 * 테이블이 만들어져야하고 테이블과 1:1 관계에 있는 VO를 만들어야 한다.
 	 */
 	public Member selectMember(String id) {
 		Member m = null;
@@ -51,7 +52,7 @@ public class jdbcTest {
 			
 			// TODO 5.statement 객체 생성
 			Statement stmt = con.createStatement();
-			String sql = "select * from member where id = " + " ' " + id + " ' "; // id만
+			String sql = "select * from member"; // id만
 																					// 동적으로
 
 			// TODO 6.sql 실행
@@ -68,15 +69,57 @@ public class jdbcTest {
 //				rs.getString(1);
 //				String id2 = rs.getString(1);//로컬중복
 				String name = rs.getString("name");
-				String password = rs.getString("age");
+				String password = rs.getString("password");
 				int age = rs.getInt("age");
 				m = new Member(id, name, password, age);
 			}
-//			if(rs.next()){}
-//			int count = rs.getInt(1); 
+
+		} catch (SQLException e) {
+			// TODO 자동 생성된 catch 블록
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();// 할당이 되어야지만 닫아야지 없는데 닫으면 null 에러
+			} catch (SQLException e) {
+				// TODO 자동 생성된 catch 블록
+				e.printStackTrace();
+			}
+		}
+
+		return m;
+	}
+	public Member selectMember() {
+		Member m = null;
+		Connection con = null;// 로컬 초기
+		try {
+			// TODO 4.DB연결
+			con = DriverManager.getConnection(url, user, password);// 컨넥셕을 가져옴
 			
-			// con.close();//다 쓰면 무조건 항상 절대! close를 해줘야함 , 자원반납안하면 나중에 error가
-			// 발생(예외도 아니고!) ,에러터지면 여기 실행안됨
+			// TODO 5.statement 객체 생성
+			Statement stmt = con.createStatement();
+			String sql = "select * from member"; // id만
+																					// 동적으로
+
+			// TODO 6.sql 실행
+			/*
+			 * dml = inset , delete, update 는 executeUpdate() sql 반환값은 int , 예를
+			 * 들어 insert 했을 때 생성된 레코드의 수
+			 * select는 executeQuery()를 사용, 결과집합[ResultSet]이라는 객체의 형태로 
+			 */
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			//TODO 7.실행 
+			while(rs.next()){
+//				rs.getString(1);
+				String id = rs.getString("id");//로컬중복
+				String name = rs.getString("name");
+				String password = rs.getString("password");
+				int age = rs.getInt("age");
+				m = new Member(id, name, password, age);
+			}
+
 		} catch (SQLException e) {
 			// TODO 자동 생성된 catch 블록
 			e.printStackTrace();
@@ -93,15 +136,61 @@ public class jdbcTest {
 		return m;
 	}
 
-	/**
-	 * 여기서 컴파일에 에러가 나면 오라클 설치나 경로 등에 문제가 있는 것
-	 * 
-	 * @param args
-	 */
+	public ArrayList<Member> selectAllMember(){
+		ArrayList<Member> allMembers = new ArrayList<>();
+		
+		Connection con = null;// 로컬 초기
+		
+		
+		try {
+			// TODO 4.DB연결
+			con = DriverManager.getConnection(url, user, password);// 컨넥셕을 가져옴
+			
+			// TODO 5.statement 객체 생성
+			Statement stmt = con.createStatement();
+			String sql = "select * from member"; // id만
+			ResultSet rs = stmt.executeQuery(sql);
+			//결과 받아옴
+			
+			//TODO 7.실행 
+			while(rs.next()){
+				
+				String id = rs.getString("id"); 
+				String name = rs.getString("name");
+				String password = rs.getString("password");
+				int age = rs.getInt("age");
+				
+				Member m = new Member(id, name, password, age);
+				allMembers.add(m);
+			}
+
+		} catch (SQLException e) {
+			// TODO 자동 생성된 catch 블록
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();// 할당이 되어야지만 닫아야지 없는데 닫으면 null 에러
+			} catch (SQLException e) {
+				// TODO 자동 생성된 catch 블록
+				e.printStackTrace();
+			}
+		}
+
+		
+		
+		return allMembers; 
+	}
+
 	public static void main(String[] args) {
 		jdbcTest test = new jdbcTest();
-		Member m = test.selectMember("test"); 
+		Member m = test.selectMember("2"); 
 		System.out.println(m);
+		
+		ArrayList<Member> alm = test.selectAllMember();
+		for (Member e : alm) {
+			System.out.println(e);
+		}
 	}
 
 }
