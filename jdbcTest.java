4c6c05ca43eedc18bc0e8d9bf3c864ba0a738374
김완길 <watakumo@gmail.com>
2016-05-19 09:02:00 +0900

    정리완료

--- jdbcTest.java
+++ jdbcTest.java
@@ -16,58 +16,59 @@ public class jdbcTest {
 	private String user = "hr";
 	private String password = "hr";
 
+	/**
+	 * 생성자에서 Driver 로딩만 해주면 된다. 아래 코드는 Driver와 Connection의 관계를 설명하기 위해 메소드처럼 적은
+	 * 것이다.
+	 */
 	public jdbcTest() {
-		// TODO 1.JDBC 드라이버 로딩
-		try {
-			Class.forName(driver);// 클래스를 로딩한다. static으로 만들어진 객체가 생성되서 등록뭔소리
-			System.out.println("드라이버 로딩 성공!");
+//		Connection con = null;//
 
-			// TODO 2.sql.Connection을 가져와야한다.
-			Connection con = DriverManager.getConnection(url, user, password);
+		
+			// TODO 1단계_JDBC 드라이버 로딩
+			// 드라이버 로딩 : arg의 경로의 라이브러리 클래스인 OracleDriver 클래스파일을 로딩
+			// 클래스 로더를 통해 OrcaleDriver의 클래스변수(static)를 읽어들이는 역할
+			try {
+				Class.forName(driver);
+				System.out.println("드라이버 로딩 성공!");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-			/*
-			 * 하나의 드라이버 로딩 커넥션 받아서 커넥션으로부터 질의문을 던져온다 (by Connection 객체로부터
-			 * Statement 객체를 만들어서, Connection의 createStatement()메소드 사용)
-			 */
-			System.out.println("DB연결성공");
-		} catch (ClassNotFoundException | SQLException e) {
-			// 둘 다 컴파일 타입 예외
-			e.printStackTrace();
-		} // 클래스 로더를 통해 클래스변수(?)를 읽어들이는 역할
-
+		 
 	}
 
 	/**
-	 * 조회하고 조회된 결과를 반환하는 메소드333
+	 * SELECT 메소드 DB에 접근해 sql을
 	 * 
-	 * @param id
-	 * 테이블이 만들어져야하고 테이블과 1:1 관계에 있는 VO를 만들어야 한다.
+	 * @param id_파라미터는
+	 *            찾을 데이터의 id를 직접 지정하는 경우 사용
+	 * @return Member객체_DB(XE)의 Member Table의 VO를 틀로해서 생성한 인스턴스(힙메모리에 로딩)
 	 */
 	public Member selectMember(String id) {
 		Member m = null;
 		Connection con = null;// 로컬 초기
 		try {
-			// TODO 4.DB연결
+			
+			// TODO 1단계_DB연결
 			con = DriverManager.getConnection(url, user, password);// 컨넥셕을 가져옴
+			System.out.println("DB접속 성공");
 			
-			// TODO 5.statement 객체 생성
+			// TODO 2단계_statement 객체 생성
+			// statement는 Connection정보를 바탕으로 Query의 
 			Statement stmt = con.createStatement();
-			String sql = "select * from member"; // id만
-																					// 동적으로
- 
-			// TODO 6.sql 실행
-			/*
-			 * dml = inset , delete, update 는 executeUpdate() sql 반환값은 int , 예를
-			 * 들어 insert 했을 때 생성된 레코드의 수
-			 * select는 executeQuery()를 사용, 결과집합[ResultSet]이라는 객체의 형태로 
-			 */
-			
+			String sql = "select * from member where id = " + "'" + id + "'"; // 동적제어
+
+			// TODO 3단계_.sql 실행[select]해서 그 결과를 ResultSet에 담기 
+			// 데이터조작어[DML]인 inset , delete, update 는 executeUpdate() sql 반환값은 int 
+			// 데이터질의어[DQL]인 select는 executeQuery()를 사용
+			// 그리고 결과집합[ResultSet]이라는 참조변수에 담음
 			ResultSet rs = stmt.executeQuery(sql);
-			
-			//TODO 7.실행 
-			while(rs.next()){
-//				rs.getString(1);
-//				String id2 = rs.getString(1);//로컬중복
+
+			// TODO 4단계_DB에서(ResultSet이라는 객체를 통해) 레코드불러오기 
+			while (rs.next()) {
+				// rs.getString(1);
+				// String id2 = rs.getString(1);//로컬중복
 				String name = rs.getString("name");
 				String password = rs.getString("password");
 				int age = rs.getInt("age");
@@ -75,60 +76,12 @@ public class jdbcTest {
 			}
 
 		} catch (SQLException e) {
-			// TODO 자동 생성된 catch 블록
 			e.printStackTrace();
 		} finally {
 			try {
 				if (con != null)
 					con.close();// 할당이 되어야지만 닫아야지 없는데 닫으면 null 에러
 			} catch (SQLException e) {
-				// TODO 자동 생성된 catch 블록
-				e.printStackTrace();
-			}
-		}
-
-		return m;
-	}
-	public Member selectMember() {
-		Member m = null;
-		Connection con = null;// 로컬 초기
-		try {
-			// TODO 4.DB연결
-			con = DriverManager.getConnection(url, user, password);// 컨넥셕을 가져옴
-			
-			// TODO 5.statement 객체 생성
-			Statement stmt = con.createStatement();
-			String sql = "select * from member"; // id만
-																					// 동적으로
-
-			// TODO 6.sql 실행
-			/*
-			 * dml = inset , delete, update 는 executeUpdate() sql 반환값은 int , 예를
-			 * 들어 insert 했을 때 생성된 레코드의 수
-			 * select는 executeQuery()를 사용, 결과집합[ResultSet]이라는 객체의 형태로 
-			 */
-			
-			ResultSet rs = stmt.executeQuery(sql);
-			
-			//TODO 7.실행 
-			while(rs.next()){
-//				rs.getString(1);
-				String id = rs.getString("id");//로컬중복
-				String name = rs.getString("name");
-				String password = rs.getString("password");
-				int age = rs.getInt("age");
-				m = new Member(id, name, password, age);
-			}
-
-		} catch (SQLException e) {
-			// TODO 자동 생성된 catch 블록
-			e.printStackTrace();
-		} finally {
-			try {
-				if (con != null)
-					con.close();// 할당이 되어야지만 닫아야지 없는데 닫으면 null 에러
-			} catch (SQLException e) {
-				// TODO 자동 생성된 catch 블록
 				e.printStackTrace();
 			}
 		}
@@ -136,57 +89,49 @@ public class jdbcTest {
 		return m;
 	}
 
-	public ArrayList<Member> selectAllMember(){
+	public ArrayList<Member> selectAllMember() {
 		ArrayList<Member> allMembers = new ArrayList<>();
-		
+
 		Connection con = null;// 로컬 초기
-		
-		
+
 		try {
-			// TODO 4.DB연결
 			con = DriverManager.getConnection(url, user, password);// 컨넥셕을 가져옴
-			
-			// TODO 5.statement 객체 생성
+
 			Statement stmt = con.createStatement();
 			String sql = "select * from member"; // id만
 			ResultSet rs = stmt.executeQuery(sql);
-			//결과 받아옴
-			
-			//TODO 7.실행 
-			while(rs.next()){
-				
-				String id = rs.getString("id"); 
+			// 결과 받아옴
+
+			while (rs.next()) {
+
+				String id = rs.getString("id");
 				String name = rs.getString("name");
 				String password = rs.getString("password");
 				int age = rs.getInt("age");
-				
+
 				Member m = new Member(id, name, password, age);
 				allMembers.add(m);
 			}
 
 		} catch (SQLException e) {
-			// TODO 자동 생성된 catch 블록
 			e.printStackTrace();
 		} finally {
 			try {
 				if (con != null)
 					con.close();// 할당이 되어야지만 닫아야지 없는데 닫으면 null 에러
 			} catch (SQLException e) {
-				// TODO 자동 생성된 catch 블록
 				e.printStackTrace();
 			}
 		}
 
-		
-		
-		return allMembers; 
+		return allMembers;
 	}
 
 	public static void main(String[] args) {
 		jdbcTest test = new jdbcTest();
-		Member m = test.selectMember("2"); 
+		Member m = test.selectMember("2");
 		System.out.println(m);
-		
+
 		ArrayList<Member> alm = test.selectAllMember();
 		for (Member e : alm) {
 			System.out.println(e);
@@ -194,14 +139,3 @@ public class jdbcTest {
 	}
 
 }
-
-
-
-/*1. 컨넥션은 한번만
- * 2. 디비연결 getconnection , 리턴타입은 connection 인터페이스 객체 
- * statements를 connection으로부터 받아오고 sql 준비해서 
- * 4. sql실행 
- * 만든 statement로 메소드 실행하는데 select일 때만 다르다. 나머지는 executeㅕpdate의 리턴타입은 int 
- * 
- * */
- 
---
+++ jdbcTest2.java
@@ -0,0 +1,8 @@
+
+public class jdbcTest2 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
